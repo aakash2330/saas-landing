43adbf2446e9d9cb97e8681f970b6b34c74fdd25
Aakash  Singh <aakashsingh@Aakashs-MacBook-Pro.local>
2024-09-10 15:06:39 +0530

    build fix

--- components/product-showcase.tsx
+++ components/product-showcase.tsx
@@ -1,28 +1,5 @@
 "use client";
-
-import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
-import appScreen from "../public/assets/Image3.png";
-import { useMediaQuery } from "../hooks/use-media-query";
-import Image from "next/image";
-
 export function ProductShowcase() {
-  const appImageRef = useRef<HTMLImageElement | null>(null);
-  const { isMobile } = useMediaQuery();
-
-  const scaleDimensions = () => {
-    return isMobile ? [0.7, 0.9] : [1.05, 1];
-  };
-
-  const { scrollYProgress } = useScroll({
-    target: appImageRef,
-    offset: ["start end", "end end"],
-  });
-
-  const rotateX = useTransform(scrollYProgress, [0, 1], [15, 0]);
-  const scale = useTransform(scrollYProgress, [0, 1], scaleDimensions());
-  const opacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
-
   return (
     <div className="bg-black bg-gradient-to-b from-black to-[#5D2CA8] py-[72px] flex justify-center items-center w-full text-white sm:py-24">
       <div className="container">
